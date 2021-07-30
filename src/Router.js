563ad2f55d53316fef8d0f5cb97828e4b6726fe8
Derek-94 <ghdtjrrl94@gmail.com>
2021-07-30 12:09:35 +0900

    refactor: Refactor codes.
    - Add Router component, that will be used in index.js.
    - Make mockData, in type of Object.
    - Remove unusing files. (img, svg... files), modified HTML.

---
+++ src/Router.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+class Router extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world!</h1>
+        {/* <Route exact path="/" component={} />
+        <Route path="/product" component={} />
+        <Route path="/recentList" component={} /> */}
+      </div>
+    );
+  }
+}
+
+export default Router;
