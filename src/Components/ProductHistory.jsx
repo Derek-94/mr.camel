2690174293e0e8346b2ccca6fcba18e6ed8037f5
Derek <52649378+Derek-94@users.noreply.github.com>
2021-07-31 20:01:00 +0900

    Revert "feat(brand): Make brand filters." (#17)
    - revert.

--- src/Components/ProductHistory.jsx
+++ src/Components/ProductHistory.jsx
@@ -1,40 +1,21 @@
 import React, { Component } from 'react';
-import mockData from '../data/mockData';
 
 export default class ProductHistory extends Component {
-  constructor() {
+  constructor(props) {
     super();
     localStorage.setItem('productHistory', JSON.stringify(tmp));
 
     this.state = {
       productHistoryOrigin: JSON.parse(localStorage.getItem('productHistory')) || [],
       productHistoryModified: [],
+      brandSelected: [],
       ignoreFlag: false,
-      checkedBrand: [],
-      allBrand: [],
       sortValue: 'recent,',
     };
   }
 
-  componentDidMount() {
-    const setBrandList = () => {
-      const sortBrand = mockData.map(brands => {
-        return brands.brand;
-      });
-      const brandsArr = new Set(sortBrand);
-      const brandList = [...brandsArr];
-      this.setState({
-        allBrand: brandList,
-      });
-    };
-    setBrandList();
-  }
-
   shouldComponentUpdate(nextState) {
     if (this.state.productHistoryModified !== nextState.productHistoryModified) {
-      return true;
-    }
-    if (this.state.checkedBrand !== nextState.checkedBrand) {
       return true;
     }
   }
@@ -54,40 +35,6 @@ export default class ProductHistory extends Component {
     }
   };
 
-<<<<<<< HEAD
-  handleAllCheck = e => {
-    const { allBrand, productHistoryOrigin } = this.state;
-    if (e.target.checked) {
-      this.setState({
-        checkedBrand: allBrand,
-        productHistoryModified: productHistoryOrigin,
-      });
-    } else {
-      this.setState({
-        checkedBrand: [],
-      });
-    }
-  };
-
-  handleSingleCheck = e => {
-    const { checkedBrand, productHistoryModified, productHistoryOrigin } = this.state;
-
-    if (productHistoryModified.length === 0) {
-      this.setState({
-        productHistoryModified: productHistoryOrigin,
-      });
-    }
-
-    if (e.target.checked) {
-      this.setState({
-        checkedBrand: [...checkedBrand, e.target.value],
-      });
-    } else {
-      const unCheck = e.target.value;
-      this.setState({
-        checkedBrand: checkedBrand.filter(brand => brand !== unCheck),
-      });
-=======
   onChangeSort = e => {
     const sortTargetValue = e.target.value;
     const { productHistoryOrigin } = this.state;
@@ -119,84 +66,50 @@ export default class ProductHistory extends Component {
         break;
       default:
         console.log('no default');
->>>>>>> 8a65526129c2032db65d7284691eba00df0a680f
     }
   };
 
   render() {
-<<<<<<< HEAD
-    const { allBrand, checkedBrand, productHistoryOrigin, productHistoryModified } = this.state;
-=======
     const { productHistoryOrigin, productHistoryModified, sortValue } = this.state;
->>>>>>> 8a65526129c2032db65d7284691eba00df0a680f
     return (
       <>
         <h1>사용자 상품 조회 이력</h1>
         <section>
-          <div className="check_brand">
+          <div>
             브랜드
-            <label className="check_all">
-              <input
-                checked={checkedBrand === allBrand ? true : false}
-                onChange={e => this.handleAllCheck(e)}
-                type="checkbox"
-                name="전체"
-                value="전체"
-              />
-              전체 브랜드
+            <label>
+              <input type="checkbox" name="스톤아일랜드" value="스톤아일랜드" />
+              스톤아일랜드
             </label>
-            {allBrand.map((el, idx) => {
-              return (
-                <label className="check_each">
-                  <input
-                    checked={checkedBrand.includes(allBrand[idx]) ? true : false}
-                    onChange={e => this.handleSingleCheck(e)}
-                    type="checkbox"
-                    name={el}
-                    value={el}
-                  />
-                  {el}
-                </label>
-              );
-            })}
+            <label>
+              <input type="checkbox" name="polo" value="polo" />
+              polo
+            </label>
           </div>
-
           <div>
             <label>
               <input onClick={this.onClickIgnoreBox} type="checkbox" name="ignore" value="ignore" />
               관심없는 제품 제외하기
             </label>
           </div>
-<<<<<<< HEAD
-
-          <select>
-            <option>최신순</option>
-            <option>낮은 가격</option>
-=======
           <select value={sortValue} onChange={this.onChangeSort}>
             <option value="dateRecent">최신순</option>
             <option value="priceAscend">낮은 가격 순서</option>
             <option value="priceDescend">높은 가격 순서</option>
->>>>>>> 8a65526129c2032db65d7284691eba00df0a680f
           </select>
         </section>
-
         {productHistoryModified.length ? (
           <div>
             <h2>상품</h2>
-            {productHistoryModified.map((product, index) => {
-              return (
-                checkedBrand.includes(product.brand) && (
-                  <div key={index} style={{ margin: '1rem 0' }}>
-                    <div>상품명: {product.title}</div>
-                    <div>브랜드: {product.brand}</div>
-                    <div>가격: {product.price}</div>
-                    <div>{product.ignore ? `관심없음` : `관심있음`}</div>
-                    <hr />
-                  </div>
-                )
-              );
-            })}
+            {productHistoryModified.map((product, index) => (
+              <div key={index} style={{ margin: '1rem 0' }}>
+                <div>상품명: {product.title}</div>
+                <div>브랜드: {product.brand}</div>
+                <div>가격: {product.price}</div>
+                <div>{product.ignore ? `관심없음` : `관심있음`}</div>
+                <hr />
+              </div>
+            ))}
           </div>
         ) : (
           ``
@@ -221,6 +134,7 @@ export default class ProductHistory extends Component {
     );
   }
 }
+
 const tmp = [
   {
     title: '중고 나이키 테아 흰검 245 30000원',
@@ -250,32 +164,8 @@ const tmp = [
     date: '',
     ignore: false,
   },
-  {
-    title: '나이키 윈드러너 블랙 L',
-    brand: '나이키',
-    price: 60000,
-    date: '',
-    ignore: true,
-  },
-  {
-    title: '나이키바람막이',
-    brand: '나이키',
-    price: 68000,
-    date: '',
-    ignore: true,
-  },
-  {
-    title: '구찌 정품 카드지갑 (급처)',
-    brand: '구찌',
-    price: 100000,
-    date: '',
-    ignore: false,
-  },
-  {
-    title: '나이키 트레이닝 바람막이',
-    brand: '나이키',
-    price: 75000,
-    date: '',
-    ignore: true,
-  },
+  { title: '나이키 윈드러너 블랙 L', brand: '나이키', price: 60000, date: '', ignore: true },
+  { title: '나이키바람막이', brand: '나이키', price: 68000, date: '', ignore: true },
+  { title: '구찌 정품 카드지갑 (급처)', brand: '구찌', price: 100000, date: '', ignore: false },
+  { title: '나이키 트레이닝 바람막이', brand: '나이키', price: 75000, date: '', ignore: true },
 ];
