1c5054cd1f232b3869aaba7006a2b014024f48eb
Derek <52649378+Derek-94@users.noreply.github.com>
2021-07-31 09:48:40 +0900

    feat(ignoreFlag): Update filtering ignore flag. (#9)
    - User clicks ignore checkbox, data below renders differently.
    - Not revise original arrays.
    - Whenever user clicks filter, page renders productHistoryModified, not
      productHistoryOrign.

--- src/Components/ProductHistory.jsx
+++ src/Components/ProductHistory.jsx
@@ -6,23 +6,40 @@ export default class ProductHistory extends Component {
     localStorage.setItem('productHistory', JSON.stringify(tmp));
 
     this.state = {
-      productHistory: JSON.parse(localStorage.getItem('productHistory')) || [],
+      productHistoryOrigin: JSON.parse(localStorage.getItem('productHistory')) || [],
+      productHistoryModified: [],
       brandSelected: [],
+      ignoreFlag: false,
     };
   }
 
-  shouldComponentUpdate() {
-    // 만약 this.state의 brandSelected가 이전과 달랄졌다면, return true
+  shouldComponentUpdate(nextState) {
+    if (this.state.productHistoryModified !== nextState.productHistoryModified) {
+      return true;
+    }
   }
 
-  render() {
-    const { productHistory } = this.state;
+  onClickIgnoreBox = () => {
+    const { ignoreFlag, productHistoryOrigin } = this.state;
+    if (!ignoreFlag) {
+      this.setState({
+        productHistoryModified: productHistoryOrigin.filter(product => !product.ignore),
+        ignoreFlag: true,
+      });
+    } else {
+      this.setState({
+        productHistoryModified: productHistoryOrigin,
+        ignoreFlag: false,
+      });
+    }
+  };
 
+  render() {
+    const { productHistoryOrigin, productHistoryModified } = this.state;
     return (
       <>
         <h1>사용자 상품 조회 이력</h1>
         <section>
-          {/* 브랜드, 노관심, 최신순, 낮은가격 */}
           <div>
             브랜드
             <label>
@@ -36,7 +53,7 @@ export default class ProductHistory extends Component {
           </div>
           <div>
             <label>
-              <input type="checkbox" name="ignore" value="ignore" />
+              <input onClick={this.onClickIgnoreBox} type="checkbox" name="ignore" value="ignore" />
               관심없는 제품 제외하기
             </label>
           </div>
@@ -45,19 +62,37 @@ export default class ProductHistory extends Component {
             <option>낮은 가격</option>
           </select>
         </section>
-        {productHistory ? (
+        {productHistoryModified.length ? (
           <div>
-            <h2>여기에 이제 렌더링 할거에요!</h2>
-            {productHistory.map(product => (
-              <div style={{ margin: '1rem 0' }}>
+            <h2>상품</h2>
+            {productHistoryModified.map((product, index) => (
+              <div key={index} style={{ margin: '1rem 0' }}>
                 <div>상품명: {product.title}</div>
                 <div>브랜드: {product.brand}</div>
                 <div>가격: {product.price}</div>
+                <div>{product.ignore ? `관심없음` : `관심있음`}</div>
+                <hr />
               </div>
             ))}
           </div>
         ) : (
-          <h2>사용자가 아무것도 보질 않았어요!</h2>
+          ``
+        )}
+        {productHistoryOrigin.length && !productHistoryModified.length ? (
+          <div>
+            <h2>상품</h2>
+            {productHistoryOrigin.map((product, index) => (
+              <div key={index} style={{ margin: '1rem 0' }}>
+                <div>상품명: {product.title}</div>
+                <div>브랜드: {product.brand}</div>
+                <div>가격: {product.price}</div>
+                <div>{product.ignore ? `관심없음` : `관심있음`}</div>
+                <hr />
+              </div>
+            ))}
+          </div>
+        ) : (
+          !productHistoryModified.length && <h2>사용자가 아무것도 보질 않았어요!</h2>
         )}
       </>
     );
