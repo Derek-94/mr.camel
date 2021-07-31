4631ae534c03f7c5a54563aadb2f759af7731e58
blingblin-g <luvbliny@gmail.com>
2021-07-31 20:00:31 +0900

    feat(utils) : about LocalStorage
    	implement functions managing localStorage

---
+++ src/utils.js
@@ -0,0 +1,32 @@
+export const getProductData = async () => {
+  try {
+    const data = await fetch('product_data.json');
+    const jsonData = await data.json();
+    return jsonData;
+  } catch (e) {
+    console.log('error: json file load fail');
+  }
+};
+
+export const getLocalStorage = key => {
+  const data = localStorage.getItem(key);
+  return data;
+};
+
+export const getRecentHistory = () => {
+  return getLocalStorage('productHistory');
+};
+
+export const setLocalStorage = (key, value) => {
+  localStorage.setItem(key, value);
+};
+
+export const setRecentHistory = value => {
+  setLocalStorage('productHistory', value);
+};
+
+export const getRandomNumber = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
