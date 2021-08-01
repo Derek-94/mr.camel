e8ca348fd5375b5c89605f857f7e16273ec806a1
hyojin <gywlsh274@gmail.com>
2021-08-01 09:45:44 +0900

    docs(README.md): 리드미 업데이트
    - 리드미 업데이트

--- src/Router.js
+++ src/Router.js
@@ -2,16 +2,13 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Products from './Components/Products';
 import Product from './Components/Product';
-
-import Home from './Home';
 import ProductHistory from './Components/ProductHistory';
 
 class Router extends Component {
   render() {
     return (
       <div>
-        <Route exact path="/" component={Home} />
-        <Route path="/product" exact component={Products} />
+        <Route path="/" exact component={Products} />
         <Route path="/product/:id" exact component={Product} />
         <Route path="/recentList" component={ProductHistory} />
       </div>
